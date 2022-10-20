16fc1068f9b4219e599630444f190bee13256ca5
Dhanush S <dhanushsiva2000@gmail.com>
2022-10-20 17:06:12 +0530

    changes

--- FoodBox BackEnd(Spring)/src/main/java/com/foodbox/controller/ProductController.java
+++ FoodBox BackEnd(Spring)/src/main/java/com/foodbox/controller/ProductController.java
@@ -45,15 +45,6 @@ public class ProductController {
 			if (adminList.isEmpty()) {
 				adminRepository.save(new Admin("admin", "password"));
 			}
-//            addProdIfEmpty(new Product(1,"Butter Chicken","Chicken infused with butter and spices","Indian",350,0,0,"yes","./assets/images/ButterChicken.png"));
-//            addProdIfEmpty(new Product(2,"Chicken Biryani","Rice Steamed with Chicken and spices","Indian",365,10,0,"yes","./assets/images/biryani.jpg"));
-//            addProdIfEmpty(new Product(3,"Steamed Mince Bun","Steamed Bun with lamb mince","Chinese",250,20,0,"yes","./assets/images/buns.jpg"));
-//            addProdIfEmpty(new Product(4,"Egg Fried Rice","Rice with Egg and Chinese sauses","Chinese",95,5,0,"yes","./assets/images/EggfriedRice.jpg"));
-//            addProdIfEmpty(new Product(2,"Paneer Pizza","Pizza topped with cotted cheese and vegies","Italian",435,0,0,"yes","./assets/images/paneerpizza.jpg"));
-//            addProdIfEmpty(new Product(2,"Red Sause Pasta","Pasta with Tomato and oregano","Italian",435,0,0,"yes","./assets/images/redPasta.jpg"));
-//            addProdIfEmpty(new Product(2,"Ravioli","Ravioli pasta filled with veg mince","Italian",200,18,0,"yes","./assets/images/ravioli.jpg"));
-//            addProdIfEmpty(new Product(2,"Elote de Corn","Corn topped with cream cheese and spice","Mexican",180,7,0,"yes","./assets/images/elote.jpg"));
-//            addProdIfEmpty(new Product(2,"Burrito","Wrapped Tortilla with Meat mince and Mayo","Mexican",350,0,0,"yes","./assets/images/Burrito.jpg"));
 			prodList = productRepository.findIfAvail();
 		}
 		return prodList;
@@ -105,7 +96,7 @@ public class ProductController {
 	@DeleteMapping("/products/{id}")
 	public ResponseEntity<Map<String, Boolean>> deleteProduct(@PathVariable Long id) {
 		Product product = productRepository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Employee Not Found with " + id));
+				.orElseThrow(() -> new ResourceNotFoundException("Product Not Found with " + id));
 		productRepository.delete(product);
 		Map<String, Boolean> map = new HashMap<>();
 		map.put("deleted", Boolean.TRUE);
